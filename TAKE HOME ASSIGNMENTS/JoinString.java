90d6df5fe1f060460dc2352f4e60819db9c16c2f
Perucia <davyncjh@gmail.com>
2023-02-21 00:36:09 +0800

    Update JoinString.java

--- TAKE HOME ASSIGNMENTS/JoinString.java
+++ TAKE HOME ASSIGNMENTS/JoinString.java
@@ -41,20 +41,18 @@ class TLL {
 
 public class JoinString {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out);
+        Kattio k = new Kattio(System.in, System.out);
         ArrayList<TLL> list = new ArrayList<>();
-        int cases = Integer.parseInt(br.readLine());
+        int cases = k.getInt();
         for (int i = 0; i < cases; i++) {
             TLL tll = new TLL();
-            tll.addFront(br.readLine());
+            tll.addFront(k.getWord());
             list.add(tll);
         }
         int first = 0;
         for (int j = 0; j < cases-1; j++) {
-            String[] temp = br.readLine().split(" ");
-            first = Integer.parseInt(temp[0])-1;
-            int second = Integer.parseInt(temp[1])-1;
+            first = k.getInt()-1;
+            int second = k.getInt()-1;
             TLL firstList = list.get(first);
             TLL secondList = list.get(second);
             firstList.tail.setNext(secondList.head);
@@ -62,9 +60,9 @@ public class JoinString {
         }
         StringNode node = list.get(first).head;
         while (node != null) {
-            pw.print(node.getItem());
+            k.print(node.getItem());
             node = node.getNext();
         }
-        pw.flush();
+        k.flush();
     }
 }
