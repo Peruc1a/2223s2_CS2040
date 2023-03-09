fc4488189a95da4aba295e7877a15526d0df6fc9
Perucia <davyncjh@gmail.com>
2023-03-09 09:22:41 +0800

    Create AssigningWorkstations.java

---
+++ 1-DAY ASSIGNMENTS/AssigningWorkstations.java
@@ -0,0 +1,47 @@
+//Davin Chua A0234351N
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class AssigningWorkstations {
+    public static void main(String[] args) {
+        Kattio k = new Kattio(System.in, System.out);
+        int rs = k.getInt();
+        int lockTime = k.getInt();
+        int counter = 0;
+        ArrayList<Researcher> arr = new ArrayList<Researcher>();
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for (int i = 0; i < rs; i++) {
+            arr.add(new Researcher(k.getInt(), k.getInt()));
+        }
+        Collections.sort(arr);
+        for (Researcher r : arr) {
+            int a = r.getArrive();
+            while (pq.size() > 0) {
+                int b = pq.peek();
+                if (a < b) break;
+                else if (a-lockTime <= b) {
+                    pq.poll();
+                    counter++;
+                    break;
+                }
+                else pq.poll();
+            }
+            pq.add(r.leaveTime());
+        }
+        System.out.println(counter);
+    }
+}
+
+class Researcher implements Comparable<Researcher> {
+    int arrive;
+    int usage;
+    public Researcher(Integer a, Integer s) {
+        arrive = a;
+        usage = s;
+    }
+    
+    public int compareTo(Researcher a) {return Integer.compare(arrive, a.arrive);}
+    public int leaveTime() { return arrive+usage;}
+    public int getArrive() { return arrive;}
+}
