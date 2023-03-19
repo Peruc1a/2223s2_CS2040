691c017d61554653a0e88e16fdd5dd24d96593fc
Perucia <davyncjh@gmail.com>
2023-03-20 02:41:48 +0800

    Create UFDS.java

---
+++ PERSONAL DATA STRUCTURES/UFDS.java
@@ -0,0 +1,34 @@
+public class UFDS {
+    public int[] p;
+    public int[] rank;
+
+    public UFDS(int N) {
+        p = new int[N];
+        rank = new int[N];
+        for (int i = 0; i < N; i++) {
+            p[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int findSet(int i) {
+        if (p[i] == i) return i;
+        else {
+            p[i] = findSet(p[i]);
+            return p[i];
+        }
+    }
+
+    public boolean isSameSet(int i, int j) { return findSet(i) == findSet(j);}
+
+    public void unionSet(int i, int j) {
+        if (!isSameSet(i, j)) {
+            int x = findSet(i), y = findSet(j);
+            if (rank[x] > rank[y]) p[y] = x;
+            else {
+                p[x] = y;
+                if (rank[x] == rank[y]) rank[y] = y+1;
+            }
+        }
+    }
+}
