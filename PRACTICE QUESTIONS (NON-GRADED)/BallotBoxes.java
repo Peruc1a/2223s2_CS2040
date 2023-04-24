3c3781d213c703405c50247c7902143be4fc9488
Perucia <davyncjh@gmail.com>
2023-04-25 04:37:20 +0800

    Update BallotBoxes.java

--- PRACTICE QUESTIONS (NON-GRADED)/BallotBoxes.java
+++ PRACTICE QUESTIONS (NON-GRADED)/BallotBoxes.java
@@ -1,36 +1,32 @@
 //Davin Chua A0234351N
+import java.util.*;
 
 public class BallotBoxes {
     public static void main(String[] args) {
         Kattio k = new Kattio(System.in, System.out);
         while (true) {
-            int cities = k.getInt();
-            int boxes = k.getInt();
-            if (cities == -1 && boxes == -1) break;
-            else if (cities == 1) {
-                k.println((int) Math.ceil(k.getDouble() / boxes));
-                continue;
+            int c = k.getInt();
+            int b = k.getInt();
+            if (c == -1) break;
+            int[] arr = new int[c];
+            int high = 0;
+            for (int i = 0; i < c; i++) {
+                int temp = k.getInt();
+                arr[i] = temp;
+                high = high > temp ? high : temp;
             }
-            int[] arr = new int[500000];
-            int low = 1, high = 0, mid;
-            for (int i = 0; i < cities; i++) {
-                arr[i] = k.getInt();
-                if (arr[i] > high) high = arr[i];
-            }
+            int low = 1;
             while (low < high) {
-                mid = (high + low) / 2;
-                int counter = 0;
-                for (int j = 0; j < cities; j++) {
-                    counter += (int) Math.ceil((double) arr[j] / mid);
-                }
-                if (counter > boxes) low = mid+1;
+                int mid = (low+high) / 2;
+                int count = 0;
+                for (int j = 0; j < c; j++) count += (arr[j]+mid-1) / mid;
+                if (count > b) low = mid+1;
                 else high = mid;
             }
             k.println(low);
         }
         k.flush();
     }
-    
 }
 
 
